c6e002166c99abe79d453885afada8c2ed06fa2b
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-12-25 16:05:40 +0000

    Send message to topic added

--- src/main/java/com/apachkafka/api/services/ProductService.java
+++ src/main/java/com/apachkafka/api/services/ProductService.java
@@ -3,14 +3,17 @@ package com.apachkafka.api.services;
 import com.apachkafka.api.models.Product;
 import com.apachkafka.api.publishers.ProductPublisher;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.UUID;
-
+import java.util.concurrent.CompletableFuture;
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ProductService {
@@ -27,10 +30,24 @@ public class ProductService {
     	String productId = UUID.randomUUID().toString(); 
         product.setId(productId);
         
+        
+        
         // Sent to the topic.
-        kafkaTemplate.send(productCreatedEventTopic, productId, product);
+        CompletableFuture<SendResult<String, Product>> sendProcess = kafkaTemplate.send(productCreatedEventTopic, productId, product);
         //publisher.sendProductToTopic(product);
 
+        /*
+        // When we whant to see the message on the topic.
+        sendProcess.whenComplete((result, exception) ->{
+        	
+        	if (exception != null)
+        		log.error("Message was not sent to kafka topic. {}", exception.getMessage());
+        	else
+        		log.info("Message was successfully set to topic. {}", result.getRecordMetadata());
+        });
+        
+        sendProcess.join();
+        */
         return product;
     }
 
